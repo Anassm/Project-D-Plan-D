dd8effb1a3fd8149b747c571fb2aa69a0e499de2
Anass Moussadi <a.moussadi03@gmail.com>
2025-03-30 14:14:52 +0200

    change loaddb script to avoid duplicates

--- Backend/prisma/LoadDB/loaddb.ts
+++ Backend/prisma/LoadDB/loaddb.ts
@@ -3,34 +3,33 @@ import dotenv from "dotenv";
 import { getDMMF } from "@prisma/internals";
 import * as fs from "fs";
 import * as path from "path";
-import * as readline from 'readline';
+import * as readline from "readline";
 dotenv.config();
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: Number(process.env.DATABASE_PORT),
-  });
+  user: "postgres",
+  host: "localhost",
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: Number(process.env.DATABASE_PORT),
+});
 
-  async function getFirstLine(filePath: string): Promise<string> {
-    const fileStream = fs.createReadStream(filePath); // Efficient file reading
-    const rl = readline.createInterface({ input: fileStream });
-    
-    for await (const line of rl) {
-      // Clean the line by removing any special characters and replacing spaces
-      const cleanedLine = line
-        .trim() // Remove leading and trailing whitespace
-        .replace(/[^\w,]/g, '') // Remove any special characters (except commas)
-        .replace(/\s+/g, '_'); // Replace spaces with underscores
-      
-      return cleanedLine; // Reads the first line and stops
-    }
-    
-    return ''; // In case the file is empty
+async function getFirstLine(filePath: string): Promise<string> {
+  const fileStream = fs.createReadStream(filePath); // Efficient file reading
+  const rl = readline.createInterface({ input: fileStream });
+
+  for await (const line of rl) {
+    // Clean the line by removing any special characters and replacing spaces
+    const cleanedLine = line
+      .trim() // Remove leading and trailing whitespace
+      .replace(/[^\w,]/g, "") // Remove any special characters (except commas)
+      .replace(/\s+/g, "_"); // Replace spaces with underscores
+
+    return cleanedLine; // Reads the first line and stops
   }
-  
+
+  return ""; // In case the file is empty
+}
 
 const getModelNames = async (): Promise<string[]> => {
   // Read the schema.prisma file
@@ -62,10 +61,16 @@ async function main() {
         const firstLine = await getFirstLine(filepath); // Read the first line
 
         // const firstLine: string = await getFirstLine(filepath);
-        console.log(firstLine)
-        const Query : string = `COPY ${element} (${firstLine}) FROM '${filepath}' WITH CSV HEADER`;
-        console.log(Query);
-        await client.query(Query);
+        console.log(firstLine);
+        const fillQuery: string = `COPY ${element} (${firstLine}) FROM '${filepath}' WITH CSV HEADER`;
+        console.log(fillQuery);
+
+        // drop table data
+        const dropQuery: string = `TRUNCATE TABLE ${element};`;
+        await client.query(dropQuery);
+
+        // fill table data
+        await client.query(fillQuery);
       }
     } catch (err) {
       console.log(err);
