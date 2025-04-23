40aa2b0bf03a7ba24a6a5c7c569612d0d20efe9d
Anass Moussadi <a.moussadi03@gmail.com>
2025-04-23 22:31:34 +0200

    demo front-end finalized

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,26 +1,27 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import showRoutes from "./routes/touchpointsRoutes";
-
 import { flightsRoutes } from "./routes/flightsRoutes";
-
 import dotenv from "dotenv";
 import authentication from "./plugins/authentication";
 
 dotenv.config();
+const backendPort: number = Number(process.env.API_PORT);
 
 export const server = Fastify();
+server.register(cors, {
+  origin: "http://localhost:5173",
+  credentials: true,
+});
 server.register(authentication);
 server.register(showRoutes);
 server.register(flightsRoutes);
 
-const port: Number = 3000;
-
-
-server.listen({ port: Number(process.env.API_PORT) }, function (err, address) {
+server.listen({ port: backendPort }, function (err, address) {
   if (err) {
-    server.log.error(err);
+    console.error(err);
     process.exit(1);
   }
 
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${backendPort}`);
 });
